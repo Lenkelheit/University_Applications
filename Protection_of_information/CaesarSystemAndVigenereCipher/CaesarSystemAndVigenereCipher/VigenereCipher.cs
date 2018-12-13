f0e03b8bd011819dcfc1b517c3fa073765e8a1f3
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-12-13 20:05:30 +0200

    Add files via upload

---
+++ Protection_of_information/CaesarSystemAndVigenereCipher/CaesarSystemAndVigenereCipher/VigenereCipher.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CaesarSystemAndVigenereCipher
+{
+    public class VigenereCipher
+    {
+        // CONST
+        private const char FIRST_LETTER_OF_ALPHABET = 'A';
+        private const char LAST_LETTER_OF_ALPHABET = 'Z';
+
+        // PROPERTIES
+        public Dictionary<char, int> Cipher { get; private set; }
+
+        // CONSTRUCTORS
+        public VigenereCipher()
+        {
+            Cipher = new Dictionary<char, int>();
+            int j = 0;
+            for (char i = FIRST_LETTER_OF_ALPHABET; i <= LAST_LETTER_OF_ALPHABET; ++i)
+            {
+                Cipher.Add(i, j++);
+            }
+        }
+
+        // METHODS
+        public void EncryptFile(string filePathFrom, string filePathTo, string keyWord)
+        {
+            if (!File.Exists(filePathFrom))
+            {
+                throw new FileNotFoundException($"File {filePathFrom} isn`t found.");
+            }
+            string text = "";
+            using (StreamReader streamReader = new StreamReader(filePathFrom))
+            {
+                text = streamReader.ReadToEnd();
+            }
+            using (StreamWriter streamWriter = new StreamWriter(filePathTo))
+            {
+                text = text.ToUpper();
+                keyWord = keyWord.ToUpper();
+                int j = 0;
+                char elem = ' ';
+                for (int i = 0; i < text.Length; ++i)
+                {
+                    if (Cipher.ContainsKey(text[i]))
+                    {
+                        // cyclically shifts keyWord
+                        j %= keyWord.Length;
+                        elem = (char)(FIRST_LETTER_OF_ALPHABET + ((Cipher[text[i]] + Cipher[keyWord[j]]) % Cipher.Count));
+
+                        streamWriter.Write(elem);
+                        ++j;
+                    }
+                    else if (text[i] == ' ')
+                    {
+                        streamWriter.Write(text[i]);
+                    }
+                }
+            }
+        }
+
+        public string DecryptFile(string filePathFrom, string keyWord)
+        {
+            if (!File.Exists(filePathFrom))
+            {
+                throw new FileNotFoundException($"File {filePathFrom} isn`t found.");
+            }
+            string text = "";
+            using (StreamReader streamReader = new StreamReader(filePathFrom))
+            {
+                text = streamReader.ReadToEnd();
+            }
+            StringBuilder decryptedText = new StringBuilder();
+            text = text.ToUpper();
+            keyWord = keyWord.ToUpper();
+            int j = 0;
+            char elem = ' ';
+            for (int i = 0; i < text.Length; ++i)
+            {
+                if (Cipher.ContainsKey(text[i]))
+                {
+                    // cyclically shifts keyWord
+                    j %= keyWord.Length;
+                    elem = (char)(FIRST_LETTER_OF_ALPHABET + ((Cipher[text[i]] + Cipher.Count - Cipher[keyWord[j]]) % Cipher.Count));
+
+                    decryptedText.Append(elem);
+                    ++j;
+                }
+                else if (text[i] == ' ')
+                {
+                    decryptedText.Append(text[i]);
+                }
+            }
+            return decryptedText.ToString();
+        }
+    }
+}
---
+++ Protection_of_information/CaesarSystemAndVigenereCipher/MainProgram/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using CaesarSystemAndVigenereCipher;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filePathText = @"..\..\TextFiles\Text.txt";
+                string filePathEncryptedTextCaesar = @"..\..\TextFiles\EncryptedTextCaesar.txt";
+                string filePathStandartFrequency = @"..\..\TextFiles\Harry Potter.txt";
+                string filePathEncryptedTextVigenere = @"..\..\TextFiles\EncryptedTextVigenere.txt";
+
+                CaesarSystem caesarSystem = new CaesarSystem();
+
+                Console.Write("Input keyword: ");
+                string keyword = Console.ReadLine();
+                Console.Write("Input key: ");
+                int key = int.Parse(Console.ReadLine());
+
+                caesarSystem.EncryptFileWithKey(filePathText, filePathEncryptedTextCaesar, keyword, key);
+
+                foreach (var elem in caesarSystem.Cipher)
+                {
+                    Console.WriteLine($"\"{elem.Key}\" - \"{elem.Value}\"");
+                }
+                Console.WriteLine();
+
+                string decryptedTextCaesar = caesarSystem.DecryptFileWithKey(filePathEncryptedTextCaesar);
+
+                FrequencyAnalysis frequency = new FrequencyAnalysis();
+                frequency.CreateFrequencyDictionary(filePathStandartFrequency);
+                foreach (var elem in frequency.StandartFrequencyDictionary.OrderBy(pair => pair.Value))
+                {
+                    Console.WriteLine($"\"{elem.Key}\" - \"{elem.Value}\"");
+                }
+
+                Console.WriteLine();
+                string decryptedTextFrequencyAnalysis = frequency.DecryptFile(filePathEncryptedTextCaesar);
+                foreach (var elem in frequency.EncryptedFrequencyDictionary.OrderBy(pair => pair.Value))
+                {
+                    Console.WriteLine($"\"{elem.Key}\" - \"{elem.Value}\"");
+                }
+
+                Console.WriteLine($"Decrypted text by Caesar system with keyword and key:\n{decryptedTextCaesar}\n");
+                Console.WriteLine($"Decrypted text by frequency analysis:\n{decryptedTextFrequencyAnalysis}\n");
+
+                VigenereCipher vigenereCipher = new VigenereCipher();
+                vigenereCipher.EncryptFile(filePathText, filePathEncryptedTextVigenere, keyword);
+
+                string decryptedTextVigenereCipher = vigenereCipher.DecryptFile(filePathEncryptedTextVigenere, keyword);
+                Console.WriteLine($"Decrypted text by Vigenere cipher:\n{decryptedTextVigenereCipher}\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
