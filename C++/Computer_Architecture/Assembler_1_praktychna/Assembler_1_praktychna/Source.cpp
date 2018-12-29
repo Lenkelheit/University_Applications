665c4f4aca9b9c5c14d882d387b44d20d5b97508
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-12-29 23:30:03 +0200

    Update Source.cpp

--- C++/Computer_Architecture/Assembler_1_praktychna/Assembler_1_praktychna/Source.cpp
+++ C++/Computer_Architecture/Assembler_1_praktychna/Assembler_1_praktychna/Source.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 using namespace std;
 
-//для Hello world
+//РґР»СЏ Hello world
 char FORMAT_1[] = "%s %s\n";
 char HELLO[] = "Hello";
 char WORLD[] = "world";
 
-//Design by Nazariy Tymtsiv, 2017; тут year - масив char`ів
+//Design by Nazariy Tymtsiv, 2017; С‚СѓС‚ year - РјР°СЃРёРІ char`С–РІ
 char FORMAT_2[] = "%s %s %s %s\n";
 char Design[] = "Design";
 char by[] = "by";
 char Nazariy[] = "Nazariy Tymtsiv,";
 char year[] = "2017";
 
-//Design by Nazariy Tymtsiv, 2017; тут year_int - int
+//Design by Nazariy Tymtsiv, 2017; С‚СѓС‚ year_int - int
 int year_int = 2017;
-char FORMAT_4[] = "%s %s %s %d\n";//%d - специфікатор ціле десяткове зі знаком
+char FORMAT_4[] = "%s %s %s %d\n";//%d - СЃРїРµС†РёС„С–РєР°С‚РѕСЂ С†С–Р»Рµ РґРµСЃСЏС‚РєРѕРІРµ Р·С– Р·РЅР°РєРѕРј
 
 void main()
 {
 	__asm
-	{	// початок асемблерного коду
+	{	// РїРѕС‡Р°С‚РѕРє Р°СЃРµРјР±Р»РµСЂРЅРѕРіРѕ РєРѕРґСѓ
 		//Hello world
-		mov eax, offset WORLD//присвоюю регістру eax значення змінної WORLD за адресою
-		push eax//заношу значення в стек
+		mov eax, offset WORLD//РїСЂРёСЃРІРѕСЋСЋ СЂРµРіС–СЃС‚СЂСѓ eax Р·РЅР°С‡РµРЅРЅСЏ Р·РјС–РЅРЅРѕС— WORLD Р·Р° Р°РґСЂРµСЃРѕСЋ
+		push eax//Р·Р°РЅРѕС€Сѓ Р·РЅР°С‡РµРЅРЅСЏ РІ СЃС‚РµРє
 		mov eax, offset HELLO
 		push eax
 		mov eax, offset FORMAT_1
 		push eax
 		mov edi, printf
-		call edi //через edi викликаю функцію printf
+		call edi //С‡РµСЂРµР· edi РІРёРєР»РёРєР°СЋ С„СѓРЅРєС†С–СЋ printf
 
-		// Очищую стек для коректного завершення процедури main
+		// РћС‡РёС‰СѓСЋ СЃС‚РµРє РґР»СЏ РєРѕСЂРµРєС‚РЅРѕРіРѕ Р·Р°РІРµСЂС€РµРЅРЅСЏ РїСЂРѕС†РµРґСѓСЂРё main
 		pop ebx
 		pop ebx
 		pop ebx
 
-		//Design by Nazariy Tymtsiv, 2017; тут year - масив char`ів
-		mov eax, offset year//присвоюю регістру eax значення змінної year за адресою
-		push eax//заношу значення в стек
+		//Design by Nazariy Tymtsiv, 2017; С‚СѓС‚ year - РјР°СЃРёРІ char`С–РІ
+		mov eax, offset year//РїСЂРёСЃРІРѕСЋСЋ СЂРµРіС–СЃС‚СЂСѓ eax Р·РЅР°С‡РµРЅРЅСЏ Р·РјС–РЅРЅРѕС— year Р·Р° Р°РґСЂРµСЃРѕСЋ
+		push eax//Р·Р°РЅРѕС€Сѓ Р·РЅР°С‡РµРЅРЅСЏ РІ СЃС‚РµРє
 		mov eax, offset Nazariy
 		push eax
 		mov eax, offset by
@@ -48,19 +48,19 @@ void main()
 		mov eax, offset FORMAT_2
 		push eax
 
-		call printf//викликаю функцію printf
+		call printf//РІРёРєР»РёРєР°СЋ С„СѓРЅРєС†С–СЋ printf
 
-		// Очищую стек для коректного завершення процедури main
+		// РћС‡РёС‰СѓСЋ СЃС‚РµРє РґР»СЏ РєРѕСЂРµРєС‚РЅРѕРіРѕ Р·Р°РІРµСЂС€РµРЅРЅСЏ РїСЂРѕС†РµРґСѓСЂРё main
 		pop ebx
 		pop ebx
 		pop ebx
 		pop ebx
 		pop ebx
 
-		//Design by Nazariy Tymtsiv, 2017; тут year_int - int
-		mov eax, year_int//присвоюю регістру eax значення змінної year_int
-		push eax//заношу значення в стек
-		mov eax, offset Nazariy//присвоюю регістру eax значення змінної WORLD за адресою
+		//Design by Nazariy Tymtsiv, 2017; С‚СѓС‚ year_int - int
+		mov eax, year_int//РїСЂРёСЃРІРѕСЋСЋ СЂРµРіС–СЃС‚СЂСѓ eax Р·РЅР°С‡РµРЅРЅСЏ Р·РјС–РЅРЅРѕС— year_int
+		push eax//Р·Р°РЅРѕС€Сѓ Р·РЅР°С‡РµРЅРЅСЏ РІ СЃС‚РµРє
+		mov eax, offset Nazariy//РїСЂРёСЃРІРѕСЋСЋ СЂРµРіС–СЃС‚СЂСѓ eax Р·РЅР°С‡РµРЅРЅСЏ Р·РјС–РЅРЅРѕС— WORLD Р·Р° Р°РґСЂРµСЃРѕСЋ
 		push eax
 		mov eax, offset by
 		push eax
@@ -69,15 +69,15 @@ void main()
 		mov eax, offset FORMAT_4
 		push eax
 
-		call printf//викликаю функцію printf
+		call printf//РІРёРєР»РёРєР°СЋ С„СѓРЅРєС†С–СЋ printf
 
-		// Очищую стек для коректного завершення процедури main
+		// РћС‡РёС‰СѓСЋ СЃС‚РµРє РґР»СЏ РєРѕСЂРµРєС‚РЅРѕРіРѕ Р·Р°РІРµСЂС€РµРЅРЅСЏ РїСЂРѕС†РµРґСѓСЂРё main
 		pop ebx
 		pop ebx
 		pop ebx
 		pop ebx
 		pop ebx
 
-	} // кінець асемблерного коду
+	} // РєС–РЅРµС†СЊ Р°СЃРµРјР±Р»РµСЂРЅРѕРіРѕ РєРѕРґСѓ
 	system("pause");
 }
